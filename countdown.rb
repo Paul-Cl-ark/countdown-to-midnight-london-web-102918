27ebdfc7936ccb07e8fc3a898c7a6e8f6c73dfc7
Paul Clark <clark.paul123@gmail.com>
2018-10-18 17:20:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,10 @@ end
 
 def countdown_with_sleep(delay)
   sleep delay
+    counter = secs
+  while counter != 0 
+    puts "#{counter} SECOND(S)!"
+      counter -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
